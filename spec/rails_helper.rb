35ea08c3d5509cbb5ad827d1b9212ee11af8a72e
Lito White <leland.white@gmail.com>
2021-04-13 14:04:40 -0600

    Add setup for Shoulda Matchers

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -33,6 +33,13 @@ end
 RSpec.configure do |config|
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
+  
+  Shoulder::Matchers.configure do |config|
+    config.integrate do |with|
+      with.test_framework :rspec
+      with.library :rails
+    end
+  end
 
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
